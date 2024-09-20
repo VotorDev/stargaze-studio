855d1480b77f9b4d695bcdb0e4fffce22c89e2d9
VotorDev <95104301+VotorDev@users.noreply.github.com>
2024-09-20 11:46:34 +0100

    Update FormGroup.tsx flex-col in smaller screens

--- components/FormGroup.tsx
+++ components/FormGroup.tsx
@@ -10,7 +10,7 @@ export const FormGroup = (props: FormGroupProps) => {
   const { title, subtitle, children } = props
 
   return (
-    <div className="flex p-4 space-x-4 w-full">
+    <div className="flex flex-col sm:flex-row p-4 space-x-4 w-full">
       {title && (
         <div className="flex flex-col w-1/3">
           <label className="flex flex-col space-y-1">
