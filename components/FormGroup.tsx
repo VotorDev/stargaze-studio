d57c49583eaf3ad6f21207ea65a128a14e8f2281
VotorDev <95104301+VotorDev@users.noreply.github.com>
2024-09-20 12:57:17 +0100

    Update FormGroup.tsx

--- components/FormGroup.tsx
+++ components/FormGroup.tsx
@@ -12,14 +12,14 @@ export const FormGroup = (props: FormGroupProps) => {
   return (
     <div className="flex flex-col sm:flex-row p-4 space-x-4 w-full">
       {title && (
-        <div className="flex flex-col w-1/3">
+        <div className="flex flex-col sm:w-1/3">
           <label className="flex flex-col space-y-1">
             <span className="font-bold">{title}</span>
             {subtitle && <span className="text-sm text-white/50">{subtitle}</span>}
           </label>
         </div>
       )}
-      <div className="space-y-4 w-2/3">{children}</div>
+      <div className="space-y-4 sm:w-2/3">{children}</div>
     </div>
   )
 }
